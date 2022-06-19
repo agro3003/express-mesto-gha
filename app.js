7a6888f20fd0528224dbef08b7a7625fada3d5c9
Roman Kononov <doubleempty@yandex.ru>
2022-06-20 01:06:27 +0300

    1

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(express.json());
 app.use(function access(req, res, next) {
   const { origin } = req.headers;
 
-  if (allowedCors.includes(origin)) res.header('Access-Control-Allow-Origin', origin);
+  if (allowedCors.includes(origin)) res.header('Access-Control-Allow-Origin', '*');
 
   const { method } = req;
 
